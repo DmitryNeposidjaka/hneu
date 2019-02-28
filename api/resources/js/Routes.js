37d26a77e98a633a3ed88257160b8902d0b07e3c
dmitry-neposidyaka <dmitry.neposidyaka@uadevelopers.com>
2019-02-28 13:44:24 +0200

    admin implement

---
+++ api/resources/js/Routes.js
@@ -0,0 +1,73 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import Layout from './components/Layout/Layout';
+import TypographyPage from './pages/Typography/Typography';
+import TablesBasicPage from './pages/Basic/Basic';
+import GoogleMapPage from './pages/Google/Google';
+import AnalyticsPage from './pages/Dashboard/Dashboard';
+import ChartsPage from './pages/Charts/Charts';
+import Login from './pages/Login/Login';
+import ErrorPage from './pages/Error/Error';
+import IconsPage from './pages/Icons/Icons';
+import NotificationsPage from './pages/Notifications/Notifications';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'hash',
+  routes: [
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/error',
+      name: 'Error',
+      component: ErrorPage,
+    },
+    {
+      path: '/app',
+      name: 'Layout',
+      component: Layout,
+      children: [
+        {
+          path: 'dashboard',
+          name: 'AnalyticsPage',
+          component: AnalyticsPage,
+        },
+        {
+          path: 'typography',
+          name: 'TypographyPage',
+          component: TypographyPage,
+        },
+        {
+          path: 'ui/icons',
+          name: 'IconsPage',
+          component: IconsPage,
+        },
+        {
+          path: 'notifications',
+          name: 'NotificationsPage',
+          component: NotificationsPage,
+        },
+        {
+          path: 'ui/charts',
+          name: 'ChartsPage',
+          component: ChartsPage,
+        },
+        {
+          path: 'tables',
+          name: 'TablesBasicPage',
+          component: TablesBasicPage,
+        },
+        {
+          path: 'ui/maps',
+          name: 'GoogleMapPage',
+          component: GoogleMapPage,
+        },
+      ],
+    },
+  ],
+});
--- api/resources/js/app.js
+++ api/resources/js/app.js
@@ -1,18 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import axios from './axios'
-import VueAxios from 'vue-axios'
-import VueClipboard from 'vue-clipboard2'
-import BootstrapVue from 'bootstrap-vue'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'expose-loader?jQuery!jquery' // eslint-disable-line
+import 'expose-loader?$!jquery' // eslint-disable-line
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import * as VueGoogleMaps from 'vue2-google-maps';
+import VueTouch from 'vue-touch';
+import Trend from 'vuetrend';
+
+import store from './store';
+import router from './Routes';
+import App from './App';
 
 Vue.use(BootstrapVue);
-Vue.use(VueClipboard)
-Vue.use(VueAxios, axios)
+Vue.use(VueTouch);
+Vue.use(Trend);
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyB7OXmzfQYua_1LEhRdqsoYzyJOPh9hGLg',
+  },
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
+  store,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+});
