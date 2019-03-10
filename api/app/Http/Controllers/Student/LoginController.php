2e68ff58d595d0c42808a1d6239c03c642ef913d
dmitry-neposidyaka <dmitry.neposidyaka@uadevelopers.com>
2019-03-10 20:16:41 +0200

    students login test

---
+++ api/app/Http/Controllers/Student/LoginController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Student;
+
+use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    public function username()
+    {
+        return 'username';
+    }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+/*        return $this->guard()->attempt(
+            $this->credentials($request), $request->filled('remember')
+        );*/
+
+        $client = new Client();
+        $response = $client->request('POST','http://142.93.204.113/moodle/login/token.php', [
+            'form_params' => [
+                'username' => $request->input('username'),
+                'password' => $request->input('password'),
+                'service' => 'moodle_mobile_app',
+            ],
+        ]);
+        $content = $response->getBody()->getContents();
+
+        $data = json_decode($content, true);
+
+        return isset($data['token']);
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    //  Route::post();
+});
+
+Route::group(['namespace' => 'Student', 'prefix' => 'student'], function () {
+    Route::post('/login', 'LoginController@login');
+});
