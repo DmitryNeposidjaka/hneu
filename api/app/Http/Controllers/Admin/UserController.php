dff518123e2ccb678c7a0c0a94867919198afba0
dmitry-neposidyaka <dmitry.neposidyaka@uadevelopers.com>
2019-03-12 19:42:59 +0200

    moodleClient fixes

--- api/app/Http/Controllers/Admin/UserController.php
+++ api/app/Http/Controllers/Admin/UserController.php
@@ -4,8 +4,46 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AdminUserCreateRequest;
+use App\Http\Requests\AdminUserUpdateRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function getMe()
+    {
+        \MoodleClient::getConnectionName();
+        return Auth::user();
+    }
 
+    public function getAll()
+    {
+        return User::all();
+    }
+
+    public function getUser(User $user)
+    {
+        return $user;
+    }
+
+    public function create(AdminUserCreateRequest $request)
+    {
+        $user = new User($request->all());
+        $user->password = Hash::make($request->password);
+        $user->save();
+        return $user;
+    }
+
+    public function update(AdminUserUpdateRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return $user;
+    }
+
+    public function delete(User $user)
+    {
+        return $user->delete();
+    }
 }
--- api/config/moodleClient.php
+++ api/config/moodleClient.php
@@ -1,5 +1,11 @@
 <?php
 
 return [
-    'default_connection' => 'test'
+    'default_connection' => 'test',
+    'connections' => [
+        'test' => [
+            'url' => 'http://142.93.204.113',
+            'token' => ''
+        ]
+    ]
 ];
