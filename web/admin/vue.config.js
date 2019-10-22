5782ee6edd80c9d4f3625901bc3848131593914f
dmitry-neposidyaka <dmitry.neposidyaka@uadevelopers.com>
2019-10-22 12:40:49 +0300

    fix webpack

--- web/admin/vue.config.js
+++ web/admin/vue.config.js
@@ -2,5 +2,23 @@ module.exports = {
     // options...
     devServer: {
         disableHostCheck: true
+    },
+    chainWebpack: (config) => {
+        config.module
+            .rule('images')
+            .use('url-loader')
+            .tap(options => Object.assign({}, options, { name: '[name].[ext]' }));
+    },
+    css: {
+        extract: {
+            filename: 'administration/[name].css',
+            chunkFilename: 'administration/[name].css',
+        },
+    },
+    configureWebpack: {
+        output: {
+            filename: 'administration/[name].js',
+            chunkFilename: 'administration/[name].js',
+        }
     }
 }
